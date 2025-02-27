37cc6a97187fafa9a0f46191157d48ee01a9d9c1
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-27 22:49:51 +0200

    development

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Porsche from "../../public/porsche.png"
 
 const Home = () => {
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-center'>
+    <div className='w-full h-fit flex flex-col items-center justify-center'>
       <h1>On this project am learning</h1>
       <p>Next.JS</p>
       <p>TypeScript</p>
       <Link href='/about'>About</Link>
       <Link href='/about/team'>Team</Link>
+      <Image src={Porsche} alt='' />
     </div>
   )
 }
