849200bff6e474b48a501ad06e00aad50142cbc6
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-27 22:47:43 +0200

    development

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Team: React.FC = () => {
+    return (
+        <div className='bg-slate-700 w-full h-screen flex text-sky-50 flex-col items-center justify-center'>
+            <p>
+                Welcome to About page
+            </p>
+        </div>
+    )
+}
+
+export default Team
---
+++ src/app/about/team/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page: React.FC = () => {
+    return (
+        <div className='w-full h-screen flex items-center justify-center'>
+            <p>This page is in the about section and is about team members</p>
+        </div>
+    )
+}
+
+export default page
