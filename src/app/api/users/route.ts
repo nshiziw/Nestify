fba71fba88a8c4d6858e5047dc6e7c03709d112f
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-02-28 17:22:34 +0200

    development

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET() {
     const users = [
@@ -20,4 +20,14 @@ export async function GET() {
     ];
 
     return NextResponse.json(users, {status: 200});
+}
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    const newUser = { id: Date.now(), ...body };
+
+    return NextResponse.json({
+        message: "User created successfully",
+        user: newUser
+    }, {status: 200})
 }
